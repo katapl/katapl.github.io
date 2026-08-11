7fe8a101cde598d5d2bf09bfc0a961eb138e7de7
katapl <202251688+katapl@users.noreply.github.com>
2026-08-11 10:14:17 +0200

    fix style paths for github pages again

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   integrations: [react(), mdx()],
   site: 'https://katapl.github.io',
-  base: '/katapl.github.io',
+  base: '/katapl.github.io/',
 });
